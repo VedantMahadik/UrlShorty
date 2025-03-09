0590fd7c880758660706b67086de58e2c4544207
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2025-03-09 23:56:22 +0530

    feat: add vite for bundling

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'url-shorty',
+      fileName: 'index',
+      formats: ['cjs'],
+    },
+    rollupOptions: {
+      external: ['express', 'cors', 'dotenv'],
+    },
+    sourcemap: true,
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+}); 
